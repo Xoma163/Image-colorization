71e0dd9713801a9fafbe734edb374b5c05662552
andrew sha <Xoma163rus@gmail.com>
2021-03-30 13:47:22 +0400

    setup dirs

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from consts import IMAGES_ORIGINAL_PATH, IMAGES_RESIZED_COLORED_PATH, IMAGES_RES
 def load_images():
     return [ImageHandler(image) for image in os.listdir(IMAGES_ORIGINAL_PATH)[:IMAGES_COUNT]]
 
+
 def setup():
     os.makedirs(IMAGES_RESIZED_COLORED_PATH, exist_ok=True)
     os.makedirs(IMAGES_RESIZED_GRAY_PATH, exist_ok=True)
 
+
 if __name__ == '__main__':
+    setup()
 
     dataset = load_images()
     data_train = [x.get_gray_as_matrix() for x in dataset]
