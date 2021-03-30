05bc8cd9cdee8cc1278b95b20d6c5008fc664361
andrew sha <Xoma163rus@gmail.com>
2021-03-30 13:40:23 +0400

    Dataset README

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 from tensorflow.keras import layers, models
 
 from consts import LEARNING_PART, IMAGE_SIZE, EPOCHS
@@ -11,13 +12,14 @@ class NeuralNetwork:
     """
     def __init__(self):
         self.model = models.Sequential([
-            layers.Conv2D(16, (3, 3), activation='relu', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1), padding='same'),
+            layers.InputLayer(input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1)),
+            layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
             layers.MaxPooling2D((2, 2)),
             layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
             layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
             layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-            layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+            layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+            layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
             layers.UpSampling2D((2, 2)),
             layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
             layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
@@ -43,17 +45,21 @@ class NeuralNetwork:
         # test_accuracy = self.model.evaluate(x=data_test_x, y=data_test_y)
         # print('Точность: ', test_accuracy)
 
+
         test_x_image = data_test_x[1].reshape((1,) + (data_test_x[1].shape))
         predicted_image = self.model.predict(test_x_image)
         predicted_image = predicted_image.reshape(predicted_image.shape[1:]).astype(np.uint8)
         img = Image.fromarray(predicted_image)
-        img.show()
+        plt.imshow(img)
+        plt.show()
 
         test_x_image = data_test_x[1].astype(np.uint8)
         test_x_image = test_x_image.reshape(test_x_image.shape[:2])
-        img = Image.fromarray(test_x_image)
-        img.show()
+        img = Image.fromarray(test_x_image).convert('RGB')
+        plt.imshow(img)
+        plt.show()
 
         test_y_image = data_test_y[1].astype(np.uint8)
         img = Image.fromarray(test_y_image)
-        img.show()
+        plt.imshow(img)
+        plt.show()
--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - cuda_11.0.2_451.48_win10
 - cudnn-11.2-windows-x64-v8.1.0.77
 - python 3.8.3
+
 ```
 SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.0\bin;%PATH%
 SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.0\extras\CUPTI\lib64;%PATH%
@@ -14,3 +15,10 @@ SET PATH=C:\tools\cuda\bin;%PATH%
 ```
 
 
+
+### Dataset
+[Download 10 000 images](https://andrewsha.net/static/files/images_10k.zip)
+
+[Download 100 000 images](https://andrewsha.net/static/files/images_100k.zip)
+
+Put dataset images in {{IMAGES_ORIGINAL_PATH}} (see in consts)
