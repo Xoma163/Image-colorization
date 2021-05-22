2de4adc3330f67437b1dd9480bb6611be83a015c
andrew sha <Xoma163rus@gmail.com>
2021-05-22 11:31:57 +0400

    remove assert gpu

--- apps/nn/NeuralNetwork.py
+++ apps/nn/NeuralNetwork.py
@@ -21,7 +21,7 @@ from .utils import CyclePercentWriter, lead_time_writer, get_time_str, get_logge
 
 logger = get_logger(__name__)
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+# assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
 config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 class LossCallback(Callback):
